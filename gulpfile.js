9c22b858b92677c491e4966e75cfb1daf4a90a81
Marc Fernandez <marcfl@ext.massimodutti.com>
2020-02-19 12:36:05 +0100

    Add gulp with some tasks for styles compiling

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    sourcemaps = require('gulp-sourcemaps');
+
+// Task for compile styles
+function style()
+{
+    return (
+        gulp
+            .src('scss/style.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on('error', sass.logError) 
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist'))
+    );
+}
+ 
+// Expose the task by exporting it
+exports.style = style;
